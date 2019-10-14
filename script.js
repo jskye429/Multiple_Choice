e2a66e33203db4e5fd14f538a147877e988e55db
Jacqueline Cameron <jskye429@mail.com>
2019-10-14 18:36:40 -0500

    functionality is back and content just needs to be added

--- script.js
+++ script.js
@@ -6,51 +6,79 @@
 
 var secondsLeft = 10;
 
-var questions = [
-    {
+var questions = [{
         title: "What language family does English belong to?",
-        answers: [1,2,3,4],
+        answers: [1, 2, 3, 4],
         correctAnswer: 1
     },
     {
         title: "The words cow and beef are from what languages respectively?",
-        answers: [1,2,3,4],
+        answers: [1, 2, 3, 4],
         correctAnswer: 1
-    }, 
+    },
     {
         title: "What alphabet does English use?",
-        answers: [1,2,3,4],
-        correctAnswer: 2
-    },  
-    {
-        title: "Generally, why does English have French in it?",
-        answers: [1,2,3,4],
+        answers: [1, 2, 3, 4],
         correctAnswer: 2
     },
+    {
+        title: "Generally, why does English have French in it?",
+        answers: [1, 2, 3, 4],
+        correctAnswer: 2
+    }
 ]
+
 var questionIndex = 0;
 
-function timerThing(){
-    console.log(secondsLeft)
+function timerThing() {
+    $("#timer").text(secondsLeft);
     secondsLeft--;
-    if(secondsLeft === 0){
+    if (secondsLeft <= 0) {
         clearInterval(timer);
     }
-}
+};
 
-var timer = setInterval(timerThing, 500);
+var timer;
 
 var question = $("#question").text("Ready to Start the Quiz?")
 var answer = $(".correctAnswer")
 
-$(".startButton").click(function(){
+function runGame() {
+
+    $("#question").text(questions[questionIndex].title)
+    $("#answer-1").text(questions[questionIndex].answers[0])
+    $("#answer-2").text(questions[questionIndex].answers[1])
+    $("#answer-3").text(questions[questionIndex].answers[2])
+    $("#answer-4").text(questions[questionIndex].answers[3])
+};
+
+$(".startButton").click(function () {
     $("#main").show();
     $(".startButton").hide();
+    timer = setInterval(timerThing, 500);
+    runGame();
+});
+$(".answer").click(function () {
+    var guess = $(this).text();
+    if (guess === questions[questionIndex].correctAnswer) {
+        console.log("good job")
+        goodAnswers++;
 
-    
-    $("#question").text(questions[questionIndex].title)
+    } else {
+        console.log("bad")
+        badAnswers++;
+        secondsLeft = secondsLeft - 15;
+        // secondsLeft -= 15;
+        }
+
+    if (questionIndex === questions.length - 1) {
+        console.log("gameover")
+    } else {
+        questionIndex++;
+        runGame();
+    }
 })
 
-$(".answer").click(function(){
-    $(this)
-})
+function end() {
+    console.log("end func")
+}
